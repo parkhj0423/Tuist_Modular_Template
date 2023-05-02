5013d41cba9852df6ee1c2fcb8c07fde85bc9810
parkhyunwoo <50567986+parkhj0423@users.noreply.github.com>
2023-05-02 17:32:18 +0900

    Initial commit

---
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+// swiftlint: disable all
+public enum ModulePaths {
+    case feature(Feature)
+    case domain(Domain)
+    case core(Core)
+    case shared(Shared)
+    case userInterface(UserInterface)
+}
+
+public extension ModulePaths {
+    enum Feature: String {
+        case BaseFeature
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public extension ModulePaths {
+    enum Domain: String {
+        case BaseDomain
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public extension ModulePaths {
+    enum Core: String {
+        case CoreKit
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public extension ModulePaths {
+    enum Shared: String {
+        case GlobalThirdPartyLibrary
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public extension ModulePaths {
+    enum UserInterface: String {
+        case DesignSystem
+
+        func targetName(type: MicroTargetType) -> String {
+            "\(self.rawValue)\(type.rawValue)"
+        }
+    }
+}
+
+public enum MicroTargetType: String {
+    case interface = "Interface"
+    case sources = ""
+    case testing = "Testing"
+}
